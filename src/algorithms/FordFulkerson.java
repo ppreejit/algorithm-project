fbf40f7c92f4af5e144744452d08ecaf3e8ed37f
Niharika Nasam <nihanasam8@gmail.com>
2023-12-05 13:28:09 -0800

    Code Refactoring

--- src/algorithms/FordFulkerson.java
+++ src/algorithms/FordFulkerson.java
@@ -1,76 +1,58 @@
 package algorithms;
-import java.util.LinkedList; // Using the LinkedList class
+import java.util.LinkedList;
 
-import graph.SimpleGraph; // necessary imports
-import model.*; // importing the utils
+import Utils.GraphUtility;
+import graph.SimpleGraph;
+import model.*;
 
+/**
+ * This class implements the Ford-Fulkerson algorithm for calculating the maximum flow associated with a flow network
+ * It used residual graph to find augmenting paths and increase the flow.
+ * The algorithm continuously finds paths from the source to the sink with available capacity and augments
+ * the flow along these paths until no more paths can be found.
+ *
+ * @author Shriya Hireholi
+ */
 public class FordFulkerson {
 
-	// Finds the maximum flow in the given SimpleGraph using the Ford-Fulkerson algorithm
-	public double getMaxFlow(SimpleGraph simpleGraph) throws Exception {
-		ResidualGraph graph = new ResidualGraph(simpleGraph);
-		ResidualVertex source = graph.getSource();
+	/**
+	 * This method computes the maximum flow associated with a flow network represented by the instance of SimpleGraph
+	 * It uses FordFulkerson algorithm to compute the maximum flow
+	 * @param graph - an instance of simple graph
+	 * @return max flow value associated with the given graph
+	 *
+	 * @throws Exception
+	 */
+	public double getMaxFlow(SimpleGraph graph) throws Exception {
+		ResidualGraph residualGraph = new ResidualGraph(graph);
+		ResidualVertex source = residualGraph.getSource();
+		LinkedList<ResidualEdge> path;
+		// Iterate until a path from source to destination no longer exists
+		do {
+			path = GraphUtility.findPathToSink(residualGraph, source,false,0);
+			resetGraphVisitedStatus(residualGraph);
 
-		// Iterate until no path from source to sink exists
-		while (true) {
-			LinkedList<ResidualEdge> path = findPathToSink(graph, source);
-			graph.clearVisitedStatus();
-			if (path == null) {
-				break;
+			if (path != null) {
+				// Calculate the bottleneck which is minimum capacity of the path
+				double bottleneck = GraphUtility.calculateBottleneck(path);
+
+				// Update the flow on the path with the bottleneck value
+				GraphUtility.updateFlowOnPath(path, bottleneck);
 			}
-
-			// Calculate the bottleneck (minimum capacity) of the path
-			double bottleneck = calculateBottleneck(path);
-
-			// Update the flow on the path with the bottleneck value
-			updateFlowOnPath(path, bottleneck);
-		}
+		} while (path != null);
 
 		// Return the outgoing flow from the source vertex
 		return source.calculateTotalOutgoingFlow();
 	}
 
-	// Calculate the bottleneck (minimum capacity) of a given path
-	private double calculateBottleneck(LinkedList<ResidualEdge> path) {
-		double bottleneck = Double.MAX_VALUE;
-		for (ResidualEdge edge : path) {
-			double residualCapacity = edge.getResidualCapacity();
-			if (residualCapacity < bottleneck) {
-				bottleneck = residualCapacity;
-			}
-		}
-		return bottleneck;
+	/**
+	 * Resets the visited status of nodes or edges within the given residual graph.
+	 *
+	 * @param residualGraph The residual graph whose visited status needs to be reset
+	 */
+	private void resetGraphVisitedStatus(ResidualGraph residualGraph){
+		residualGraph.clearVisitedStatus();
 	}
 
-	// Find a path from the given origin vertex to the sink vertex in the ResidualGraph
-	private LinkedList<ResidualEdge> findPathToSink(ResidualGraph graph, ResidualVertex source) {
-		source.markVisited();
 
-		if (source.getIdentifier().equals(graph.getSink().getIdentifier())) {
-			LinkedList<ResidualEdge> path = new LinkedList<>();
-			return path; // Return an empty path since the source is the sink
-		}
-
-		for (ResidualEdge edge : source.getEdges()) {
-			double residualCapacity = edge.getResidualCapacity();
-			ResidualVertex destination = edge.getDestination();
-
-			if (residualCapacity > 0 && !destination.isVisited()) {
-				LinkedList<ResidualEdge> path = findPathToSink(graph, destination);
-				if (path != null) {
-					path.addFirst(edge);
-					return path;
-				}
-			}
-		}
-
-		return null;
-	}
-
-	// Increase the flow on each edge of the given path by the specified bottleneck value
-	private void updateFlowOnPath(LinkedList<ResidualEdge> path, double bottleneck) throws Exception {
-		for (ResidualEdge edge : path) {
-			edge.updateFlow(bottleneck);
-		}
-	}
 }
